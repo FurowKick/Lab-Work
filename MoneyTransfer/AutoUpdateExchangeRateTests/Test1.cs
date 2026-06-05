04d30ac6a1e84ba1c348d307bdab8a6fb9ae6f75
Ананьин <artananin@gmail.com>
2026-06-05 03:24:12 +0300

    feature: Добавлен тестовый проект для новых функций

---
+++ MoneyTransfer/AutoUpdateExchangeRateTests/Test1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoneyTransfer.Models;
+
+namespace MoneyTransfer.Tests
+{
+    [TestClass]
+    public class CurrencyConverterUpdateTests
+    {
+        private CurrencyConverter converter;
+
+        [TestInitialize]
+        public void Setup() => converter = new CurrencyConverter();
+
+        // Тестирует: Курс USD/EUR больше нуля после успешного обновления
+        // Тип: Позитивный тест
+        [TestMethod]
+        public async Task UpdateRatesAsync_OnSuccess_RateIsPositive()
+        {
+            await converter.UpdateRatesAsync();
+            Assert.IsTrue(converter.GetRate("USD", "EUR") > 0);
+        }
+
+        // Тестирует: LastUpdated обновляется после успешного запроса к API
+        // Тип: Позитивный тест
+        [TestMethod]
+        public async Task UpdateRatesAsync_OnSuccess_LastUpdatedIsSet()
+        {
+            await converter.UpdateRatesAsync();
+            Assert.AreNotEqual(DateTime.MinValue, converter.LastUpdated);
+        }
+    }
+}
